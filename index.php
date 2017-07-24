659a1edcb113fabb5e438c8aa2f741adcbd852f6
DmitriyRF <dmitriy_r_f@mail.ru>
2017-07-24 20:20:26 +0300

    Creating a Plugin File Header

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+	Plugin name: newPlugin
+	Description: Just test plugin from scratch cretor
+	Version: 1.2
+	Author: Dmitriy
+	Author URI: https://vk.com/dmitriy_r_f
+	Text Domain: newplugin
+
+ */
